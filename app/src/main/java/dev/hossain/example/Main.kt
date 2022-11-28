13bc8f0027e1a99151a564996d5637e34ad0a17b
Hossain Khan <hello@hossain.dev>
2022-11-28 14:52:03 -0500

    [ADDED] Test code to test the library

--- app/src/main/java/dev/hossain/example/Main.kt
+++ app/src/main/java/dev/hossain/example/Main.kt
@@ -1,7 +1,15 @@
 package dev.hossain.example
 
+import dev.hossain.android.catalogparser.Parser
+import dev.hossain.android.catalogparser.models.AndroidDevice
 import java.util.Date
 
 fun main() {
     println("Application run on ${Date()}")
+
+    val parser = Parser()
+    val csvFileContent = object {}.javaClass.getResourceAsStream("/android-devices-catalog.csv")!!.bufferedReader().readText()
+    val parsedDevices: List<AndroidDevice> = parser.parseDeviceCatalogData(csvFileContent)
+
+    println("Parsed ${parsedDevices.size} devices from the catalog CSV file.")
 }
