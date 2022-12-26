a057777e3e53a4e842837c5d4380b32095ddc696
Hossain Khan <hello@hossain.dev>
2022-12-26 00:16:59 -0500

    [UPDATE] Write to file

--- sample/src/main/java/dev/hossain/example/Main.kt
+++ sample/src/main/java/dev/hossain/example/Main.kt
@@ -4,6 +4,7 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import dev.hossain.android.catalogparser.Parser
 import dev.hossain.android.catalogparser.models.AndroidDevice
+import java.io.File
 import java.util.Date
 
 fun main() {
@@ -16,11 +17,15 @@ fun main() {
 
     println("Parsed ${parsedDevices.size} devices from the catalog CSV file.")
 
-    processRecordsToDb(parsedDevices.take(10))
+    processRecordsToDb(parsedDevices.take(100))
 }
 
 private fun processRecordsToDb(parsedDevices: List<AndroidDevice>) {
-    val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+    val dbFile = File("devices.db")
+    if(dbFile.exists()) {
+        dbFile.delete()
+    }
+    val driver: SqlDriver = JdbcSqliteDriver("${JdbcSqliteDriver.IN_MEMORY}devices.db")
 
     val database = DeviceDatabase(driver)
     DeviceDatabase.Schema.create(driver)
@@ -28,14 +33,17 @@ private fun processRecordsToDb(parsedDevices: List<AndroidDevice>) {
     val deviceQueries = database.deviceQueries
 
     parsedDevices.forEach { androidDevice ->
-        deviceQueries.insert(
-            brand = androidDevice.brand,
-            device = androidDevice.device,
-            manufacturer = androidDevice.manufacturer,
-            model_name = androidDevice.modelName,
-            ram = androidDevice.ram,
-            processor_name = androidDevice.processorName,
-        )
+        deviceQueries.transaction {
+            deviceQueries.insert(
+                brand = androidDevice.brand,
+                device = androidDevice.device,
+                manufacturer = androidDevice.manufacturer,
+                model_name = androidDevice.modelName,
+                ram = androidDevice.ram,
+                processor_name = androidDevice.processorName,
+            )
+        }
+
         val deviceId: Long = deviceQueries.lastInsertRowId().executeAsOne()
         println("Inserted new record with id: $deviceId")
 
