89e2a60043346ebea67bf43b7fa90bd432af0746
Hossain Khan <hello@hossain.dev>
2022-12-25 20:59:42 -0500

    [ADDED] SQL delight plugin
    Solved version issue with https://stackoverflow.com/questions/60070739/plugin-request-for-plugin-already-on-the-classpath-must-not-include-a-version
    
    Docs https://cashapp.github.io/sqldelight/1.5.4/jvm_sqlite/

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     // For build.gradle.kts (Kotlin DSL)
     // https://kotlinlang.org/docs/releases.html#release-details
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm")
 
     // https://docs.gradle.org/current/userguide/publishing_maven.html
     // See the documentation and examples: https://docs.jitpack.io
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm")
     application
+    id("com.squareup.sqldelight")
 }
 
 group = "dev.hossain.example"
@@ -12,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(project(":lib"))
+    implementation("com.squareup.sqldelight:sqlite-driver:1.5.4")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
