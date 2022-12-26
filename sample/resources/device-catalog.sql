5e1d9e69e0f0762b70aa2470fdcd20be29ec6a14
Hossain Khan <hello@hossain.dev>
2022-12-25 22:23:04 -0500

    [UPDATE] Model table

--- sample/resources/device-catalog.sql
+++ sample/resources/device-catalog.sql
@@ -1,40 +1,45 @@
 CREATE TABLE "device" (
   "_id" INTEGER NOT NULL ON CONFLICT REPLACE PRIMARY KEY AUTOINCREMENT,
-  "brand" TEXT(256) NOT NULL,
-  "device" TEXT(256) NOT NULL,
-  "manufacturer" TEXT(256) NOT NULL,
-  "model_name" TEXT(256),
-  "ram" TEXT(128),
-  "processor_name" TEXT(256)
+  "brand" TEXT NOT NULL,
+  "device" TEXT NOT NULL,
+  "manufacturer" TEXT NOT NULL,
+  "model_name" TEXT NOT NULL,
+  "ram" TEXT NOT NULL,
+  "processor_name" TEXT NOT NULL
 );
 
 CREATE TABLE "device_abi" (
   "device_id" INTEGER NOT NULL,
-  "abi" TEXT(256) NOT NULL,
+  "abi" TEXT NOT NULL ON CONFLICT REPLACE,
+  PRIMARY KEY ("device_id", "abi"),
   CONSTRAINT "fk_device_id" FOREIGN KEY ("device_id") REFERENCES "device" ("_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "device_opengl" (
   "device_id" INTEGER NOT NULL,
-  "opengl_version" TEXT(128) NOT NULL ON CONFLICT REPLACE,
+  "opengl_version" TEXT NOT NULL ON CONFLICT REPLACE,
+  PRIMARY KEY ("device_id", "opengl_version"),
   CONSTRAINT "fk_device_id" FOREIGN KEY ("device_id") REFERENCES "device" ("_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "device_screen_density" (
   "device_id" INTEGER NOT NULL,
-  "screen_density" integer NOT NULL ON CONFLICT REPLACE,
+  "screen_density" INTEGER NOT NULL ON CONFLICT REPLACE,
+  PRIMARY KEY ("device_id", "screen_density"),
   CONSTRAINT "fk_device_id" FOREIGN KEY ("device_id") REFERENCES "device" ("_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "device_screen_size" (
   "device_id" INTEGER NOT NULL,
-  "screen_size" TEXT(256) NOT NULL ON CONFLICT REPLACE,
+  "screen_size" TEXT NOT NULL ON CONFLICT REPLACE,
+  PRIMARY KEY ("device_id", "screen_size"),
   CONSTRAINT "fk_device_id" FOREIGN KEY ("device_id") REFERENCES "device" ("_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "device_sdk" (
   "device_id" INTEGER NOT NULL,
-  "sdk_version" integer NOT NULL ON CONFLICT REPLACE,
+  "sdk_version" INTEGER NOT NULL ON CONFLICT REPLACE,
+  PRIMARY KEY ("device_id", "sdk_version"),
   CONSTRAINT "fk_device_id" FOREIGN KEY ("device_id") REFERENCES "device" ("_id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
