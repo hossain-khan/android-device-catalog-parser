47a8b0b8c8eede723e7abe3de64e46e5536269a5
Hossain Khan <hello@hossain.dev>
2024-02-18 16:10:03 -0500

    [ADDED] [#55] Dokka doc gen
    `./gradlew lib:dokkaHtml`
    
    Closes #55

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@ plugins {
     // KtLint - An anti-bikeshedding Kotlin linter with built-in formatter
     // https://github.com/jeremymailen/kotlinter-gradle
     id("org.jmailen.kotlinter") version "4.2.0" apply false
+
+    // Dokka - Documentation Engine for Kotlin
+    // https://github.com/Kotlin/dokka
+    id("org.jetbrains.dokka") version "1.9.10"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
