2106685f8908ae01f52c9b47d4d8d29425d01dda
Hossain Khan <hello@hossain.dev>
2025-08-01 19:56:52 -0400

    [FIX] Kotlin formatter

--- lib/src/main/kotlin/dev/hossain/android/catalogparser/Parser.kt
+++ lib/src/main/kotlin/dev/hossain/android/catalogparser/Parser.kt
@@ -32,76 +32,82 @@ class Parser {
     fun parseDeviceCatalogData(csvContent: String): List<AndroidDevice> {
         val deviceRows: List<Map<String, String>> = csvReader.readAllWithHeader(csvContent)
 
-        return deviceRows.map { rowData ->
-            val brand = rowData[CSV_KEY_BRAND]
-            val device = rowData[CSV_KEY_DEVICE]
-            val manufacturer = rowData[CSV_KEY_MANUFACTURER]
-            val modelName = rowData[CSV_KEY_MODEL_NAME]
-            val ram = rowData[CSV_KEY_RAM]
-            val formFactor = rowData[CSV_KEY_FORM_FACTOR]
-            val processorName = rowData[CSV_KEY_SOC]
-            val gpu = rowData[CSV_KEY_SCREEN_GPU]
-            val screenSizes = rowData[CSV_KEY_SCREEN_SIZES]
-            val screenDensities = rowData[CSV_KEY_SCREEN_DENSITIES]
-            val abis = rowData[CSV_KEY_ABIS]
-            val sdkVersions = rowData[CSV_KEY_SDK_VERSIONS]
-            val openGlEsVersions = rowData[CSV_KEY_OPENGL_ES_VERSIONS]
+        return deviceRows
+            .map { rowData ->
+                val brand = rowData[CSV_KEY_BRAND]
+                val device = rowData[CSV_KEY_DEVICE]
+                val manufacturer = rowData[CSV_KEY_MANUFACTURER]
+                val modelName = rowData[CSV_KEY_MODEL_NAME]
+                val ram = rowData[CSV_KEY_RAM]
+                val formFactor = rowData[CSV_KEY_FORM_FACTOR]
+                val processorName = rowData[CSV_KEY_SOC]
+                val gpu = rowData[CSV_KEY_SCREEN_GPU]
+                val screenSizes = rowData[CSV_KEY_SCREEN_SIZES]
+                val screenDensities = rowData[CSV_KEY_SCREEN_DENSITIES]
+                val abis = rowData[CSV_KEY_ABIS]
+                val sdkVersions = rowData[CSV_KEY_SDK_VERSIONS]
+                val openGlEsVersions = rowData[CSV_KEY_OPENGL_ES_VERSIONS]
 
-            // Validates non-null values
-            if (brand.isNullOrBlank() ||
-                device.isNullOrBlank() ||
-                manufacturer.isNullOrBlank() ||
-                modelName.isNullOrBlank() ||
-                ram.isNullOrBlank() ||
-                formFactor.isNullOrBlank() ||
-                processorName.isNullOrBlank() ||
-                gpu.isNullOrBlank() ||
-                screenSizes.isNullOrBlank() ||
-                screenDensities.isNullOrBlank() ||
-                abis.isNullOrBlank() ||
-                sdkVersions.isNullOrBlank() ||
-                openGlEsVersions.isNullOrBlank()
-            ) {
-                return@map null
-            }
+                // Validates non-null values
+                if (brand.isNullOrBlank() ||
+                    device.isNullOrBlank() ||
+                    manufacturer.isNullOrBlank() ||
+                    modelName.isNullOrBlank() ||
+                    ram.isNullOrBlank() ||
+                    formFactor.isNullOrBlank() ||
+                    processorName.isNullOrBlank() ||
+                    gpu.isNullOrBlank() ||
+                    screenSizes.isNullOrBlank() ||
+                    screenDensities.isNullOrBlank() ||
+                    abis.isNullOrBlank() ||
+                    sdkVersions.isNullOrBlank() ||
+                    openGlEsVersions.isNullOrBlank()
+                ) {
+                    return@map null
+                }
 
-            return@map AndroidDevice(
-                brand = brand,
-                device = device,
-                manufacturer = manufacturer,
-                modelName = modelName,
-                ram = ram,
-                formFactor = formFactor,
-                processorName = processorName,
-                gpu = gpu,
-                screenSizes =
-                    screenSizes.split(CSV_MULTI_VALUE_SEPARATOR)
-                        .filter { it.isNotEmpty() }
-                        .toSortedSet()
-                        .toList(),
-                screenDensities =
-                    screenDensities.split(CSV_MULTI_VALUE_SEPARATOR)
-                        .filter { it.isNotEmpty() }
-                        .toSortedSet()
-                        .toList()
-                        .map { it.toInt() },
-                abis =
-                    abis.split(CSV_MULTI_VALUE_SEPARATOR)
-                        .filter { it.isNotEmpty() }
-                        .toSortedSet()
-                        .toList(),
-                sdkVersions =
-                    sdkVersions.split(CSV_MULTI_VALUE_SEPARATOR)
-                        .filter { it.isNotEmpty() }
-                        .toSortedSet()
-                        .toList()
-                        .map { it.toInt() },
-                openGlEsVersions =
-                    openGlEsVersions.split(CSV_MULTI_VALUE_SEPARATOR)
-                        .filter { it.isNotEmpty() }
-                        .toSortedSet()
-                        .toList(),
-            )
-        }.filterNotNull()
+                return@map AndroidDevice(
+                    brand = brand,
+                    device = device,
+                    manufacturer = manufacturer,
+                    modelName = modelName,
+                    ram = ram,
+                    formFactor = formFactor,
+                    processorName = processorName,
+                    gpu = gpu,
+                    screenSizes =
+                        screenSizes
+                            .split(CSV_MULTI_VALUE_SEPARATOR)
+                            .filter { it.isNotEmpty() }
+                            .toSortedSet()
+                            .toList(),
+                    screenDensities =
+                        screenDensities
+                            .split(CSV_MULTI_VALUE_SEPARATOR)
+                            .filter { it.isNotEmpty() }
+                            .toSortedSet()
+                            .toList()
+                            .map { it.toInt() },
+                    abis =
+                        abis
+                            .split(CSV_MULTI_VALUE_SEPARATOR)
+                            .filter { it.isNotEmpty() }
+                            .toSortedSet()
+                            .toList(),
+                    sdkVersions =
+                        sdkVersions
+                            .split(CSV_MULTI_VALUE_SEPARATOR)
+                            .filter { it.isNotEmpty() }
+                            .toSortedSet()
+                            .toList()
+                            .map { it.toInt() },
+                    openGlEsVersions =
+                        openGlEsVersions
+                            .split(CSV_MULTI_VALUE_SEPARATOR)
+                            .filter { it.isNotEmpty() }
+                            .toSortedSet()
+                            .toList(),
+                )
+            }.filterNotNull()
     }
 }
